ac000f2807e9b8b9eefe2a2bd84d16d7d099749c
Pauline Tannos <ptannos2@gmail.com>
2026-07-16 10:24:20 -0400

    Add missing vite-env.d.ts

---
+++ src/vite-env.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+declare module "*.css?inline" {
+  const content: string;
+  export default content;
+}
